f0740331f48ba5c65b66b5779e8f91523d8944a1
liyongzhen <liyongzhen@zhidianlife.com>
2019-05-30 19:23:51 +0800

    first commit”

---
+++ src/main/java/com/disp/config/MybatisPlusConfig.java
@@ -0,0 +1,55 @@
+package com.disp.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * @author zd-jsb
+ */
+//@Configuration
+public class MybatisPlusConfig {
+
+    @Autowired
+    private DataSource dataSource;
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 乐观锁
+     *
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+
+//    @Bean
+//    public MybatisSqlSessionFactoryBean sqlSessionFactory() throws Exception{
+//        MybatisSqlSessionFactoryBean sqlSessionFactoryBean= new MybatisSqlSessionFactoryBean();
+//        sqlSessionFactoryBean.setDataSource(dataSource);
+//        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:/mapper/*Dao.xml"));
+//        return sqlSessionFactoryBean;
+//    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+        scannerConfigurer.setBasePackage("com.disp.dao");
+        return scannerConfigurer;
+    }
+}
---
+++ src/main/java/com/disp/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.disp.dao;
+
+import com.disp.entity.UserEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 用户表
+ * 
+ * @author xuzl
+ * @email xzl18614035671@gmail.com
+ * @date 2019-05-30 15:15:45
+ */
+@Mapper
+public interface UserDao extends BaseMapper<UserEntity> {
+	
+}
